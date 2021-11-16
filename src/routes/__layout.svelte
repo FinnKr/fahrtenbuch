440d06b978b113005524fdb612f3420c89152dfd
Finn Kranzosch <finn@berlin-fn.de>
2021-11-16 20:23:36 +0100

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script type="ts">
+	import '../styles/tailwind-output.css';
+</script>
+
+<!-- Try some classes here -->
+<h1 class="uppercase text-center w-full text-2xl">Fahrtenbuch</h1>
+<slot/>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script type="ts">
+    interface Fahrt {
+        id: String
+        driver_id: String
+        start_km: Number
+        end_km: Number
+    }
+</script>
